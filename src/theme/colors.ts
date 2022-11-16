f977802b7ad1089e97736405a40bf87c9adc1972
Andy Megadev <andy.megadev@gmail.com>
2022-11-16 22:25:31 +0200

    Add theme and global styles

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  accent: '#6e3eff',
+  black: '#fff',
+  greyLigth: '#f5f5f6',
+  greyMid: '#cbcbcb',
+  greyDark: '#717171',
+  primary: '#fff',
+  white: '#fff'
+};
+
+export default colors;
