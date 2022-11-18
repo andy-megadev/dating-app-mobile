a243059b35523904651dccfc4208ba25fa100c57
Andy Megadev <andy.megadev@gmail.com>
2022-11-18 21:23:39 +0200

    Update fonts, styles and theme

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ const App = () => {
   const [isAppReady, setIsAppReady] = useState(false);
   const [isFontsLoaded] = useFonts({
     'Rubik-Regular': require('./src/assets/fonts/Rubik-Regular.ttf'),
+    'Rubik-Medium': require('./src/assets/fonts/Rubik-Medium.ttf'),
     'Rubik-SemiBold': require('./src/assets/fonts/Rubik-SemiBold.ttf')
   });
 
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,7 +1,8 @@
 const colors = {
   accent: '#6e3eff',
-  black: '#fff',
-  greyLigth: '#f5f5f6',
+  black: '#000',
+  blueFacebook: '#1578f2',
+  greyLight: '#d9d9d9',
   greyMid: '#cbcbcb',
   greyDark: '#717171',
   primary: '#fff',
