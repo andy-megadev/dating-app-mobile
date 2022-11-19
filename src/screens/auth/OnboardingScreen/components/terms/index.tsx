d82b93316ef98912ba9af9076d3ea3fabafe634f
Andy Megadev <andy.megadev@gmail.com>
2022-11-19 19:55:15 +0200

    Split onboarding screen into components

---
+++ src/screens/auth/OnboardingScreen/components/terms/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Button } from '../../../../../components';
+import styles from '../../styles';
+
+const Terms = () => {
+  return (
+    <View style={[styles.center, styles.termsContainer]}>
+      <View style={styles.terms}>
+        <Text style={[styles.text, styles.textPermission]}>
+          {'By signing up, you agree to our '}
+        </Text>
+        <Button
+          isTextButton={true}
+          title={'Terms and Conditions'}
+          titleStyle={[styles.text, styles.textPermission, styles.textLink]}
+        />
+      </View>
+      <View style={styles.terms}>
+        <Text style={[styles.text, styles.textPermission]}>
+          {'Learn how we use your data in our '}
+        </Text>
+        <Button
+          isTextButton={true}
+          title={'Privacy Policy'}
+          titleStyle={[styles.text, styles.textPermission, styles.textLink]}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Terms;
