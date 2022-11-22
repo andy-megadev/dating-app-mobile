1262a0a8c1948958c550d021bb20be1cedfec7f1
Andy Megadev <andy.megadev@gmail.com>
2022-11-22 19:16:16 +0200

    Add chat notification alert to onboarding screen

--- src/screens/auth/OnboardingScreen/index.tsx
+++ src/screens/auth/OnboardingScreen/index.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Continue, Gender, Logo, Terms } from './components';
 import styles from './styles';
+import i18n from '../../../i18n';
+import { alert, generateAlertConfig, IAlertConfig } from '../../../utils';
 
 const OnboardingScreen = () => {
+  const alertButtons: IAlertConfig['buttons'] = useMemo(
+    () => [
+      {
+        text: i18n.t('common.noThanks'),
+        onPress: handleOk,
+        style: 'default'
+      },
+      {
+        text: i18n.t('common.ok'),
+        onPress: handleCancel,
+        style: 'default'
+      }
+    ],
+    []
+  );
+
+  const alertConfig = useMemo(
+    () =>
+      generateAlertConfig(
+        i18n.t('onboarding.wantToBeNotified'),
+        undefined,
+        alertButtons
+      ),
+    []
+  );
+
+  useEffect(() => alert(alertConfig), []);
+
+  const handleOk = () => {};
+
+  const handleCancel = () => {};
+
   return (
     <SafeAreaView style={[styles.safeArea, styles.mainContainer]}>
       <Logo />
