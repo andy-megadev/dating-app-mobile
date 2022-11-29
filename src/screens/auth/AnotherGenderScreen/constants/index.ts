9fd74333b72a760e44fdf7c75de3c4975a4bc4f9
Andy Megadev <andy.megadev@gmail.com>
2022-11-29 22:36:44 +0200

    Implement another gender screen layout

--- src/screens/auth/AnotherGenderScreen/constants/index.ts
+++ src/screens/auth/AnotherGenderScreen/constants/index.ts
@@ -4,3 +4,9 @@ export const SHOW_TO_OPTIONS = [
   i18n.t('anotherGender.toLookingForWomen'),
   i18n.t('anotherGender.toLookingForMen')
 ];
+
+export const INTERSEX_TRAITS_OPTIONS = [
+  i18n.t('common.yes'),
+  i18n.t('common.no'),
+  i18n.t('anotherGender.idRatherNotSay')
+];
