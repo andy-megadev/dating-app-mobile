1ac2e35e37a0fe45444200084ce8ff520217d74a
Andy Megadev <andy.megadev@gmail.com>
2023-01-01 14:52:12 +0200

    Update identity types

--- src/screens/auth/AnotherGenderScreen/constants/index.ts
+++ src/screens/auth/AnotherGenderScreen/constants/index.ts
@@ -1,12 +1,14 @@
 import i18n from 'src/i18n';
 
+import { IntersexTraits, ShowTo } from '../types';
+
 export const SHOW_TO_OPTIONS = [
-  { title: i18n.t('anotherGender.toLookingForWomen'), value: 'woman' },
-  { title: i18n.t('anotherGender.toLookingForMen'), value: 'man' }
+  { title: i18n.t('anotherGender.toLookingForWomen'), value: ShowTo.W },
+  { title: i18n.t('anotherGender.toLookingForMen'), value: ShowTo.M }
 ];
 
 export const INTERSEX_TRAITS_OPTIONS = [
-  i18n.t('common.yes'),
-  i18n.t('common.no'),
-  i18n.t('anotherGender.idRatherNotSay')
+  { title: i18n.t('common.yes'), value: IntersexTraits.Y },
+  { title: i18n.t('common.no'), value: IntersexTraits.N },
+  { title: i18n.t('anotherGender.idRatherNotSay'), value: IntersexTraits.S }
 ];
